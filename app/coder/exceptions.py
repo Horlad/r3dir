f84ecc6526ae26022591cb679ffc2055942ea06a
Vladyslav Horodivskyi <vladyslav@Vladyslavs-MacBook-Pro.local>
2022-09-28 00:09:00 +0200

    initial commit

---
+++ app/coder/exceptions.py
@@ -0,0 +1,14 @@
+class BaseCoderError(Exception):
+    """Base class for r3dir encoding/decoding errors"""
+
+class TooLongTarget(BaseCoderError):
+    """Too long target to encode via domain-based redirection"""
+
+class WrongEncodedURLFormat(BaseCoderError):
+    """Wrong format of URL to decode"""
+
+class Base32DecodingError(WrongEncodedURLFormat):
+    """Base32-encoded target decoding error"""
+
+class StatusCodeNotInRangeError(WrongEncodedURLFormat):
+    """Status code is not in [200, 600) range"""
